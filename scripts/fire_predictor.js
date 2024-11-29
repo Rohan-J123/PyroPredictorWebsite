3fa79496e1772759084a544692753556176fb051
Rohan-J123 <rohanjha@aol.in>
2024-11-29 21:43:33 +0530

    Created 2 Model structure

--- scripts/fire_predictor.js
+++ scripts/fire_predictor.js
@@ -10,9 +10,30 @@ class L2 {
 }
 tf.serialization.registerClass(L2);
 
+var model_original;
+var model_modify;
+
+async function loadModels() {
+    try {
+        model_original = await tf.loadLayersModel('../Model/Forest_Fire_Predictor_3_Original/model.json');
+        model_modify = await tf.loadLayersModel('../Model/Forest_Fire_Predictor_3_Modify/model.json');
+
+        console.log("Models loaded successfully");
+        return { model_original, model_modify };
+    } catch (error) {
+        console.error('Error loading models:', error);
+        throw error;
+    }
+}
+
+loadModels();
+
 async function runModelPrediction(latitude, longitude) {
     try {
-        const model = await tf.loadLayersModel('../Model/Forest_Fire_Predictor_3/model.json');
+        if (!model_original || !model_modify) {
+            console.log("Models are not loaded yet, waiting...");
+            await loadModels();
+        }
 
         const cacheKey = `la=${latitude.toFixed(3)},lo=${longitude.toFixed(3)}`;
         const cachedParameters = JSON.parse(sessionStorage.getItem(cacheKey));
@@ -34,7 +55,7 @@ async function runModelPrediction(latitude, longitude) {
         const results = [];
         for (let i = 0; i < 7; i++) {
             const inputTensor = tf.tensor([groupedData[i]]);
-            const prediction = model.predict(inputTensor);
+            const prediction = model_modify.predict(inputTensor);
             const predictionArray = await prediction.array();
 
             if(parseFloat(predictionArray[0]) < 0){
@@ -55,12 +76,12 @@ async function runModelPrediction(latitude, longitude) {
     }
 }
 
-var weightArray = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-
 async function runModelPredictionDistrict(districtID, dateNumber) {
     try {
-        const model_original = await tf.loadLayersModel('../Model/Forest_Fire_Predictor_3_Original/model.json');
-        const model_modify = await tf.loadLayersModel('../Model/Forest_Fire_Predictor_3_Modify/model.json');
+        if (!model_original || !model_modify) {
+            console.log("Models are not loaded yet, waiting...");
+            await loadModels();
+        }
 
         const cacheKey = `districtID=${districtID}`;
         const cachedParameters = JSON.parse(localStorage.getItem(cacheKey));
@@ -80,20 +101,6 @@ async function runModelPredictionDistrict(districtID, dateNumber) {
             localStorage.setItem("Date", getDateRange(0));
         }
 
-        const firstLayerOriginal = model_original.layers[0];
-        const firstLayerModify = model_modify.layers[0];
-
-        const [weights, biases] = firstLayerOriginal.getWeights();
-        const weightsArray = await weights.array();
-        for (let i = 0; i < weightArray.length; i++) {
-            for (let j = 0; j < weightsArray[i].length; j++) {
-                weightsArray[i][j] *= weightArray[i];
-            }
-        }
-
-        const modifiedWeights = tf.tensor(weightsArray);
-        firstLayerModify.setWeights([modifiedWeights, biases]);
-
         const inputTensor = tf.tensor([groupedData[dateNumber]]);
         const prediction = model_modify.predict(inputTensor);
         const predictionArray = await prediction.array();
@@ -102,8 +109,46 @@ async function runModelPredictionDistrict(districtID, dateNumber) {
         prediction.dispose();
 
         return predictionArray[0];
+
     } catch (error) {
         console.error('Error during prediction:', error);
         throw error;
     }
+}
+
+async function modifyModelWeights(weightArray) {
+    try {
+        const firstLayerOriginal = model_original.getLayer('dense');
+        const firstLayerModify = model_modify.getLayer('dense');
+
+        const [weights, biases] = firstLayerOriginal.getWeights();
+
+        const zeroBiases = tf.zerosLike(biases);
+
+        const weightsArray = weights.arraySync();
+
+        for (let i = 0; i < weightArray.length; i++) {
+            for (let j = 0; j < weightsArray[i].length; j++) {
+                weightsArray[i][j] *= weightArray[i];
+            }
+        }
+
+        const modifiedWeights = tf.tensor(weightsArray);
+        
+        firstLayerModify.setWeights([modifiedWeights, zeroBiases]);
+    } catch (error) {
+        console.error('Error during prediction:', error);
+        throw error;
+    }
+}
+
+async function changeWeights() {
+    document.getElementById('weightModalClose').click();
+    var weightArray = [];
+    for(var i= 0; i < 20; i++){
+        weightArray.push(document.getElementById('weightSlider' + i).value);
+    }
+
+    await modifyModelWeights(weightArray);
+    await loadDistrictLayers(0);
 }
